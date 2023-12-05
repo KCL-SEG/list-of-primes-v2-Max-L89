8509659733ef156b54f1e91e74ab5ccbe0ba67a9
root <root@DESKTOP-TKAH30G>
2023-12-05 19:54:43 +0000

    fisish the exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+    else:
+        list = [2]
+        current_num = 2
+        while len(list) < number_of_primes:
+            is_primes = True
+            for prime in list:
+                if current_num % prime == 0:
+                    is_primes = False
+                    break
+
+            if is_primes == True:
+                list.append(current_num)
+
+            current_num += 1
+
+        return list
+
+print(primes(20))
